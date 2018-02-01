3aef480cb19b100bdab55e5f880b803e7f7f362a
stephxu96 <scarletxuiam0917@gmail.com>
2018-02-01 04:20:34 -0500

    Fixed encoding bug

--- index.js
+++ index.js
@@ -3,19 +3,11 @@
 // Express initialization
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+var httpProxy = require('http-proxy');
 var bodyParser = require('body-parser');
 var dotEnv = require('dotenv').config();
-const routes = require('./routes');
+//const routes = require('./routes');
 const portNum = process.env.PORT;
-
-// Set up db connection
-var MongoURL = process.env.MONGODB_URL || process.env.MONGOLAB_URL;
-var MongoClient = require('mongodb').MongoClient, format = require("util").format;
-
-var db = MongoClient.connect(MongoURL, function(error, dbConnection) {
-	db = dbConnection;
-});
 
 // Set up public path
 app.use(express.static(__dirname + '/public'));
@@ -27,8 +19,51 @@ app.use(function(request, response, next) {
 });
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Set up db connection
+var MongoClient = require('mongodb').MongoClient;
+var mongoUri = process.env.MONGODB_URL;
+var db = MongoClient.connect(mongoUri, function(error, client) {
+	if (error) {
+		console.log("MongoClient connection failed.");
+	} else {
+		db = client.db('urlencodes');
+	}
+});
+
+app.post('/api', function(req, res) {
+	if (!req.body) { 
+		return res.sendStatus(400); 
+	}
+	var encoded = convertUrl(req.body.url);
+	res.send(encoded);
+});
+
+app.use(/\/[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789]{5}/, function (req, res, next) {
+	var reqUrl = req.url;
+	var longUrl = "";
+	var shortUrl = reqUrl.substring(1);
+	db.collection('urls', function(err1, collection) {
+		if (err1) { 
+			console.log("Error opening URL collection.") 
+		}
+		else {
+			collection.findOne( {'tinyUrl': shortUrl}, function(err2, result) {
+				if (err2) { console.log("Error finding tiny url.") }
+				else if (result) {
+					//tinyUrl exists in db
+					longUrl = result['longUrl'];
+					//reroute 
+					return res.status(301).redirect("https://" + longUrl);
+				} else {
+					res.sendFile('public/404.html', {root: __dirname});
+				}
+			});
+		}
+	});
+});
 
 app.listen(portNum, () => {
 	console.log("Listening on port " + portNum);
@@ -40,9 +75,11 @@ function convertUrl(longUrl){
 	var encoded = '';
 	db.collection('urls', function(err1, collection) {
 		if (err1) { console.log("Error opening URL collection.") }
-
-		else {
-			collection.find( {"longUrl": longUrl}, function(err2, result) {
+		else if (collection == undefined) {
+			console.log("collection doesn't exist.");
+			return tinyUrl;
+		} else {
+			collection.findOne( { "longUrl": longUrl }, function(err2, result) {
 				if (err2) { console.log("Error finding long url.") }
 
 				else if (result) {
@@ -50,6 +87,7 @@ function convertUrl(longUrl){
 					return tinyUrl + result["tinyUrl"];
 				} else {
 					encoded = encodeUrl(longUrl);
+					console.log(encoded);
 					newUrls = {'longUrl': longUrl, 'tinyUrl': encoded};
 					collection.insert(newUrls, function(err3, saved) {
 						if (err3) { console.log("Error inserting new urls.") }
